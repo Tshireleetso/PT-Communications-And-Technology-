da643b1c5331436ccd30bd15a5fdaea5ca00e95a
Tshireletso Modise <137284627+Tshireleetso@users.noreply.github.com>
2026-03-07 20:20:27 +0200

    ,

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-export default defineConfig({
-  plugins: [vue()],
-base: "/PT-Communications-And-Technology-/" // must match your repo name exactly
-});
